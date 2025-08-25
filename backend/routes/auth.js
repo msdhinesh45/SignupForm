b866432520b8019b9fa2f72634df9899d40183e4
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-08-25 12:55:31 +0530

    Generate JWT token with validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/Schema");
-
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 // Register
@@ -28,13 +28,14 @@ router.post("/register", async (req, res) => {
       email,
       password,
     });
-
+const token = generateToken(user._id);
     res.status(201).json({
       id: user._id,
       firstname: user.firstname,
       lastname: user.lastname,
       mobileNumber: user.mobileNumber,
       email: user.email,
+      token,
     });
   } catch (err) {
     console.error(err);
@@ -57,18 +58,29 @@ router.post("/login", async (req, res) => {
     if (!user || !(await user.matchPassword(password))) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
-
+const token = generateToken(user._id);
     res.status(200).json({
       id: user._id,
       firstname: user.firstname,
       lastname: user.lastname,
       mobileNumber: user.mobileNumber,
       email: user.email,
+      token,
     });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+router.get("/me",protect, async(req,res)=>{
+  res.status(200).json(req.user)
+})
+
+// Generate JWT tokem
+
+const generateToken = (id)=>{
+  return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:"30d"})
+}
 
 module.exports = router;
