b3a31df2dadb7a24f927927d8fd44e70b35459fb
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-08-25 21:05:46 +0530

    Remove put api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -103,50 +103,6 @@ router.get("/me", protect, async (req, res) => {
   return res.status(200).json(req.user);
 });
 
-// update user current 
-router.put("/:id", protect, async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    // Allows only your account
-    if (id !== String(req.user._id)) {
-      return res.status(403).json({ message: "You can update only your account" });
-    }
-
-    const allowed = ["firstname", "lastname", "email", "mobileNumber", "password"];
-    const updates = {};
-    for (const k of allowed) {
-      if (k in req.body && req.body[k] !== undefined && req.body[k] !== null) {
-        updates[k] = k === "email" ? String(req.body[k]).toLowerCase() : req.body[k];
-      }
-    }
-
-    if (Object.keys(updates).length === 0) {
-      return res.status(400).json({ message: "No valid fields to update" });
-    }
-
-    const user = await User.findById(id).select("+password");
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    Object.assign(user, updates);
-    await user.save(); 
-
-    return res.status(200).json({
-      id: user._id,
-      firstname: user.firstname,
-      lastname: user.lastname,
-      mobileNumber: user.mobileNumber,
-      email: user.email,
-    });
-  } catch (err) {
-    console.error(err);
-    if (err.code === 11000) {
-      const key = Object.keys(err.keyValue)[0];
-      return res.status(400).json({ message: `${key} already in use` });
-    }
-    return res.status(500).json({ message: "Server Error" });
-  }
-});
 
 // Get current user data
  router.get("/", protect, async (_req, res) => {
