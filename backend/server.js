766b7542c675cf60aa329b4b7551a37e4679c8f4
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-08-25 13:14:44 +0530

    install Bcrypt library for password hashing

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,19 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
-
+const authRoutes = require("./routes/auth");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Api working");
-});
 
+// app.get("/", (req, res) => {
+//   res.send("Api working");
+// });
+
+app.use("/api/users", authRoutes)
 console.log("MONGO_URI:", process.env.MONGO_URI);
 
 connectDB();
