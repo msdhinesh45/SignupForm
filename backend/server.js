1fdf05be719c8f964b230a73c794927b8926b7d9
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-08-25 10:13:54 +0530

    PORT number and MONGO_URI setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-// import 'dotenv/config.js'
+'dotenv/config.js'
 
-const PORT =  5000;
+const PORT = process.env.PORT ||  5000;
 const app = express();
 
 app.use(cors());
